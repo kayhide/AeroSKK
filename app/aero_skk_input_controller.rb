9835dc87ac68ddf48c1fdcf95cc7df983baf3cb1
kayhide <kayhide@gmail.com>
2013-12-10 21:16:27 +0900

    update AeroSKKInputController#handleEvent.

--- app/aero_skk_input_controller.rb
+++ app/aero_skk_input_controller.rb
@@ -1,9 +1,17 @@
 class AeroSKKInputController < IMKInputController
-  def inputText string, client: sender
+  def write_log string
     @file ||= File.expand_path('~/aero_skk.log')
     open(@file, 'a+') do |io|
       io << "[#{Time.now}] #{string}\n"
     end
+  end
+
+  def initWithServer server, delegate: delegate, client: client
+    self.write_log 'initWithServer'
+    super
+  end
+
+  def inputText string, client: sender
     charset = NSCharacterSet.characterSetWithCharactersInString(('a'..'z').to_a.join)
     scanner = NSScanner.scannerWithString(string)
     isLatinChar = scanner.scanCharactersFromSet(charset, intoString: nil)
@@ -12,6 +20,18 @@ class AeroSKKInputController < IMKInputController
       true
     else
       false
+    end
+  end
+
+  def handleEvent event, client: sender
+    eventString = event.characters
+    keyCode = event.keyCode
+    modifierFlags = event.modifierFlags
+    self.write_log "#{eventString}, #{keyCode}, #{modifierFlags}"
+    if self.inputText eventString, client: sender
+      true
+    else
+      super
     end
   end
 end
